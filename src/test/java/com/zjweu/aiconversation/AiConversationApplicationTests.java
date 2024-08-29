89fd6c0eeeaf3426c87c39c37e437b244b6d8cc6
czt <1648082800@qq.com>
2024-08-29 10:02:29 +0800

    初始

---
+++ src/test/java/com/zjweu/aiconversation/AiConversationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjweu.aiconversation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiConversationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

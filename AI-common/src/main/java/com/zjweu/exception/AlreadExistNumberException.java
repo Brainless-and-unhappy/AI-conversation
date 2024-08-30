fd69a255ba1197425cb0b0efd97e11f56e56c604
czt <1648082800@qq.com>
2024-08-30 16:13:42 +0800

    注册

---
+++ AI-common/src/main/java/com/zjweu/exception/AlreadExistNumberException.java
@@ -0,0 +1,15 @@
+package com.zjweu.exception;
+
+/**
+ * 手机号已存在异常
+ */
+public class AlreadExistNumberException extends BaseException {
+
+    public AlreadExistNumberException() {
+    }
+
+    public AlreadExistNumberException(String msg) {
+        super(msg);
+    }
+
+}
